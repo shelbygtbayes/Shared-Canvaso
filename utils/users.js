0f38ae5f6f74d94d6d16260a9296e6594faa3586
KANISHKA TYAGI <tyagikanishka786@gmail.com>
2020-08-22 23:24:22 +0530

    Commit

---
+++ utils/users.js
@@ -0,0 +1,27 @@
+const users = [];
+function userJoin(id , username , room){
+    const user = {id , username , room};
+    users.push(user);
+    return user;
+}
+// Get the Current User
+function getCurrentUser(id){
+    return users.find(user => user.id === id); 
+}
+
+function userLeave(id) {
+    const index = users.findIndex(user => user.id === id);
+    if(index !== -1){
+        return users.splice(index , 1)[0]; // return the id
+    }
+}
+
+function getRoomUsers(room){
+    return users.filter(user => user.room === room);
+}
+module.exports = {
+    userJoin,
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
+};
